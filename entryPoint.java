ce1bb85090c4f8ba4601d3d2d7c19ad77b6c083a
Marqodes <marqodes@gmail.com>
2022-10-20 10:55:30 -0500

    Testing new inputvalidation

--- entryPoint.java
+++ entryPoint.java
@@ -2,8 +2,14 @@ public class EntryPoint
 {
     public static void main(String[] args)
     {
-        System.out.println("hello");
-        System.out.println("hello");
+        Person newPerson = new Person();
+
+        newPerson.setName();
+        newPerson.setAge();
+        newPerson.setGender();
+        newPerson.setHeight();
+        newPerson.setWeight();
+
     }
     
 }
